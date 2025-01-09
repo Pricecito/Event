4f75aef93b95c99862f870a81ceb8656ac0d9f64
Pricecito <matias.gutierrez.1254@gmail.com>
2025-01-08 20:28:09 -0500

    Services

--- src/main/java/com/usmp/event/controllers/LoginController.java
+++ src/main/java/com/usmp/event/controllers/LoginController.java
@@ -6,15 +6,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.usmp.event.persistence.models.Users;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
     @GetMapping
-    public String index(Model modelo){
+    public String index(Model modelo) {
         modelo.addAttribute("user", new Users());
         return "login/index";
     }
-    
+
+    @PostMapping
+    public String postMethodName(@RequestBody String entity) {
+        // TODO: process POST request
+
+        return entity;
+    }
+
 }
--- src/main/java/com/usmp/event/service/UserService.java
+++ src/main/java/com/usmp/event/service/UserService.java
@@ -1,6 +1,8 @@
 package com.usmp.event.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,21 @@ public class UserService {
     @Autowired
     private UsersRepository repository;
 
-    public List<Users> findAllUsers(){
-        
+    public List<Users> findAllUsers() {
         return repository.findAll();
     }
+
+    public Optional<Users> findOneUser(UUID id) {
+        return repository.findById(id);
+    }
+
+    public Users saveOrUpdateUser(Users user) {
+        repository.save(user);
+        return user;
+    }
+
+    public void deleteUser(UUID id) {
+        repository.deleteById(id);
+    }
+
 }
