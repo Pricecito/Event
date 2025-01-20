a572e175a3c03993ce2800dc91aae6856bfd1dc9
Pricecito <matias.gutierrez.1254@gmail.com>
2025-01-19 20:16:13 -0500

    registro terminado

--- src/main/java/com/usmp/event/controllers/RegistroController.java
+++ src/main/java/com/usmp/event/controllers/RegistroController.java
@@ -1,19 +1,19 @@
 package com.usmp.event.controllers;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.usmp.event.persistence.models.Users;
 import com.usmp.event.service.UserService;
 
+import io.micrometer.core.ipc.http.HttpSender;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -23,34 +23,24 @@ public class RegistroController {
     private UserService user_Service;
 
     @GetMapping
-    public String index(Model modelo, HttpSession session) {
-        if (Objects.isNull(modelo.getAttribute("viewModel"))) {
-            modelo.addAttribute("user", new Users());
-        } else {
-            modelo.addAttribute("user", modelo.getAttribute("viewModel"));
-        }
+    public String index(Model modelo) {
+        modelo.addAttribute("user", new Users());
         return "registro/index";
 
     }
 
-    /*
-     * @PostMapping
-     * public String save(Users user, RedirectAttributes redirect) {
-     * redirect.addAttribute("viewUser", user);
-     * return "redirect:/registro/confirmation";
-     * }
-     */
-
-    @GetMapping("/confirmation")
-    public String messageConfirm(/* @RequestParam("viewUser") */ Users modelView, Model modelo) {
-        modelo.addAttribute("viewModel", modelView);
-        return "confirmation/index";
+    @PostMapping
+    public String save(Users user, RedirectAttributes redirect, HttpSession session) {
+        user_Service.saveOrUpdateUser(user);
+        session.setAttribute("login", user);
+        redirect.addFlashAttribute("user", user);
+        return "redirect:/registro/afirmative";
     }
 
     @GetMapping("/afirmative")
-    public String messageCongrulations(@ModelAttribute("viewModel") Users user) {
-        user_Service.saveOrUpdateUser(user);
-        return "confirmation/message";
+    public String messageCongrulations(@ModelAttribute("user") Users user, Model modelo) {
+        modelo.addAttribute("user", user);
+        return "confirmation/index";
     }
 
 }
